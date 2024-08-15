31d94006e1555c51ac9cd954490412ffdd4ced95
laminsurachman <rahman0669@gmail.com>
2024-08-15 21:03:34 +0700

    adding Mainheader

--- react-frontend/src/user/components/UserItem.js
+++ react-frontend/src/user/components/UserItem.js
@@ -6,18 +6,16 @@ import Card from "../../shared/components/UIElements/Card";
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <Card>
+      <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
-          <div className="user-item__content">
-            <div className="user-item__image">
-              <Avatar image={props.image} alt={props.name} />
-            </div>
-            <div className="user-item__info">
-              <h2>{props.name}</h2>
-              <h3>
-                {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-              </h3>
-            </div>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
           </div>
         </Link>
       </Card>
